904e4207f193b06a35b6009689cc04adea134e6e
NizarDawood <77719056+NizarDawood@users.noreply.github.com>
2023-02-09 13:34:02 +0100

    CS1

---
+++ src/chat.java
@@ -0,0 +1,18 @@
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class chat {
+    private JTextArea textArea1;
+    private JTextField textField1;
+    private JButton button1;
+
+    public chat() {
+        button1.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                JOptionPane
+            }
+        });
+    }
+}
